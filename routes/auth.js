41b780d38cb1a31a00d4f828ee8637ab1f55cd6f
Adam Drumm <Adrummtelco@gmail.com>
2022-04-08 16:04:40 -0400

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,13 @@ const router = express.Router();
 router.get('/microsoft', passport.authenticate('microsoft'));
 router.get('/microsoft/callback',
       passport.authenticate('microsoft', {
-        successRedirect: `${process.env.FRONTEND_URL}/`,
-        failureRedirect: `${process.env.FRONTEND_URL}/login`
+        successRedirect: `${process.env.FRONTEND_DEV_URL}/`,
+        failureRedirect: `${process.env.FRONTEND_DEV_URL}/login`
       })
 );
 router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect(process.env.FRONTEND_URL);
+  res.redirect(process.env.FRONTEND_DEV_URL);
 });
 
 
