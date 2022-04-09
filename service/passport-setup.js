20aced901a8ed13d43dda7a73b696858511df1bc
Adam Drumm <Adrummtelco@gmail.com>
2022-04-08 20:59:51 -0400

    update

--- service/passport-setup.js
+++ service/passport-setup.js
@@ -22,7 +22,7 @@ passport.use(
       {
         clientID: process.env.MICROSOFT_CLIENT_ID,
         clientSecret: process.env.MICROSOFT_CLIENT_SECRET,
-        callbackURL: process.env.MICROSOFT_PROD_CALLBACK_URL,
+        callbackURL: "/auth/microsoft/callback",
         scope: ["user.read"],
       },
       async function (accessToken, refreshToken, profile, done) {
