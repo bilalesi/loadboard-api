8d1134928c9530e742f6ac950f1675df4bde7e94
bilalesi <b_meddah@esi.dz>
2022-04-13 00:09:55 +0100

    update yarn, auth-check to 202

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,7 @@ router.get("/logout", (req, res) => {
 
 const authCheck = (req, res, next) => {
   if (!req.user) {
-    res.status(401).json({
+    res.status(202).json({
       authenticated: false,
       message: "user has not been authenticated"
     });
